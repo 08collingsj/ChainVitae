efaaeeed1e807bfca9c31cacaa96f503804d4c51
Jaxovik <jlc-1996@live.co.uk>
2019-03-03 17:52:24 +0000

    Created Basic Console App including genesis block code. Split project into Console and WPF

---
+++ ChainVitae/ChainVitae_Console/ChainVitae_Console/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainVitae_Console
+{
+    public class Block
+    {
+        private int previousHash;
+        private string[] transactions;
+
+        private int blockHash;
+
+        public Block(int previousHash, string[] transactions)
+        {
+            this.previousHash = previousHash;
+            this.transactions = transactions;
+
+            object[] content = { transactions.GetHashCode(), previousHash };
+            this.blockHash = content.GetHashCode();
+        }
+
+        public int getPreviousHash()
+        {
+            return previousHash;
+        }
+
+        public string[] getTransaction()
+        {
+            return transactions;
+        }
+
+        public int getBlockHash()
+        {
+            return blockHash;
+        }
+    }
+}
---
+++ ChainVitae/ChainVitae_Console/ChainVitae_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainVitae_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a basic blockchain solution
+            ArrayList blockchain = new ArrayList();
+            string[] genesisTransactions = { "To: my first transaction", "From: my first transaction " };
+            Block genesisBlock = new Block(0, genesisTransactions);
+            Console.WriteLine("Genesis Hash: ");
+            Console.WriteLine(genesisBlock.getBlockHash());
+            Console.Read();
+        }
+    }
+}
