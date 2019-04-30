7d3136ca7a2b5cd12ca110246a27a1464552b919
WINDOWS\Q5081793 <jlc-1996@live.co.uk>
2019-04-30 16:27:05 +0100

    Unit Testing and Whisper Implementation

---
+++ ChainVitae/ChainVitae_Console/UnitTesting/NodeInstanceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using ChainVitae_Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class NodeInstanceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void CreateNodeNoParams()
+        {
+            NodeInstance n = new NodeInstance();
+            Assert.IsNotNull(n);
+        
+        }
+        [TestMethod]
+        public void CreateGenesisNode()
+        {
+            NodeInstance n = new NodeInstance();
+            Assert.IsNotNull(n);
+        }
+        [TestMethod]
+        public void CreateNodeWithBlockchain()
+        {
+            BlockChain test = new BlockChain();
+            List<NodeInstance> allNodes = new List<NodeInstance>();
+            NodeInstance m = new NodeInstance(test, allNodes);
+            Assert.IsNotNull(m);
+            Assert.IsNotNull(m.GetId);
+            Assert.IsTrue(m.GetState);
+            Assert.IsNotNull(m.GetChain);
+        }
+
+        [TestMethod]
+        public void CommunicateBetweenTwoNodes()
+        {
+            NodeInstance nodeInstance1 = new NodeInstance();
+            NodeInstance nodeInstance2 = new NodeInstance();
+            nodeInstance1.SendWhisper();
+            List<string> mempool = nodeInstance2.GetMemoryPool;
+            Assert.IsTrue(mempool.Contains("Basic Whisper"));
+            Assert.IsTrue(mempool.Count > 1);
+        }
+
+        [TestMethod]
+        public void TransactionWhisper()
+        {
+            Address a = new Address();
+            Address b = new Address();
+            NodeInstance nodeInstance1 = new NodeInstance();
+            NodeInstance nodeInstance2 = new NodeInstance();
+            TransactionWithDouble transactionWithDouble = new TransactionWithDouble(a, b, 20.0);
+            nodeInstance1.SendWhisper(transactionWithDouble);
+            List<string> mempool = nodeInstance2.GetMemoryPool;
+            Assert.IsTrue(mempool.Contains("Basic Whisper"));
+            Assert.IsTrue(mempool.Count > 1);
+        }
+        [TestMethod]
+        public void NodeListWhisper()
+        {
+
+        }
+        [TestMethod]
+        public void NewBlockWhisper()
+        {
+
+        }
+    }
+}
---
+++ ChainVitae/ChainVitae_Console/UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a507b98-56ac-4f97-8fcf-eba171ebec28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
